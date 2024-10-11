2e31c05712e6431e88888f5de4afb1060c3ead4a
rjo5364 <rjo5364@psu.edu>
2024-10-11 11:45:27 -0700

    initialized SQLite Database and added cursor for product fields

---
+++ app/src/main/java/edu/psu/sweng888/brickselector/DBHelper.java
@@ -0,0 +1,78 @@
+package edu.psu.sweng888.brickselector;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "Products.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_PRODUCTS = "products";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_PRODUCTS + "("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "name TEXT,"
+                + "description TEXT,"
+                + "seller TEXT,"
+                + "price REAL,"
+                + "image BLOB"
+                + ")";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
+
+    // Method to add a product
+    public void addProduct(Product product) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", product.getName());
+        values.put("description", product.getDescription());
+        values.put("seller", product.getSeller());
+        values.put("price", product.getPrice());
+        values.put("image", product.getImage());
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+    }
+
+    // Method to get all products
+    public List<Product> getAllProducts() {
+        List<Product> productList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_PRODUCTS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Product product = new Product(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getString(3),
+                        cursor.getDouble(4),
+                        cursor.getBlob(5)
+                );
+                productList.add(product);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return productList;
+    }
+}
